a8db70fa8a47217a10e0c30bd6f46a351107119c
lucasfrontend <lucasmartinezguardavidas@gmail.com>
2022-11-10 22:28:49 -0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "porfolio_with_vue_and_tailwind" : "/",
   transpileDependencies: true,
 
   pluginOptions: {
