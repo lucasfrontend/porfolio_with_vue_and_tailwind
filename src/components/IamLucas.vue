c501a1a5cb828c6ddee9d07d3cae43dafb7ae146
lucasfrontend <lucasmartinezguardavidas@gmail.com>
2022-11-24 17:57:16 -0300

    array del home

--- src/components/IamLucas.vue
+++ src/components/IamLucas.vue
@@ -17,7 +17,7 @@ export default {
       return {
         typeValue: '',
         typeStatus: false,
-        typeArray: ["Frontend Developer", "App Developer", "Video Editor"],
+        typeArray: ["Web Developer", "App Developer", "Video Editor"],
         typingSpeed: 100,
         erasingSpeed: 50,
         newTextDelay: 500,
